e55d0d794e00fed5e718e9e55f7ad3350ff5c2e5
Andy1iang <andyliang254@gmail.com>
2023-09-29 18:23:37 -0400

    Initial commit

---
+++ src/scenes/example.tsx
@@ -0,0 +1,27 @@
+import {makeScene2D} from '@motion-canvas/2d/lib/scenes';
+import {Circle} from '@motion-canvas/2d/lib/components';
+import {createRef} from '@motion-canvas/core/lib/utils';
+import {all} from '@motion-canvas/core/lib/flow';
+
+
+export default makeScene2D(function* (view) {
+  const myCircle = createRef<Circle>();
+
+  view.add(
+    <Circle
+      ref={myCircle}
+      // try changing these properties:
+      x={-300}
+      y={0}
+      width={140}
+      height={140}
+      fill="#e13238"
+    />,
+  );
+
+  yield* all(
+    myCircle().position.x(300, 2).to(-300, 2),
+    myCircle().position.y(300,1).to(0,1).to(-300,1).to(0,1),
+    myCircle().fill('#5232e1', 2).to('#e13238', 2),
+  );
+});
